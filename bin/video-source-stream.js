32ff7e3ff5c10e15f68b2b8a25b195e470c2dcf7
Mike Green <mike.is.green@gmail.com>
2017-05-08 19:01:13 -0400

    Rewrite video source stream executable to use VideoStream class and handle its events

--- bin/video-source-stream.js
+++ bin/video-source-stream.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-const gstreamer = require('gstreamer-superficial');
-const argv = require('yargs')
-  .option('address', {
-    alias: 'a',
+/* eslint-disable no-console */
+
+const VideoStream = require('../lib/video-stream');
+const argv        = require('yargs')
+  .option('host', {
+    alias: 'H',
     describe: 'Destination server address',
     type: 'string',
     default: '127.0.0.1',
@@ -14,23 +16,59 @@ const argv = require('yargs')
     type: 'number',
     default: 9001,
   })
+  .option('device', {
+    alias: 'd',
+    describe: 'Video source device',
+    type: 'string',
+    default: '/dev/video0',
+  })
+  .option('frames', {
+    alias: 'f',
+    describe: 'Frames per second',
+    type: 'number',
+    default: 15,
+  })
+  .option('width', {
+    alias: 'x',
+    describe: 'Video width',
+    type: 'number',
+    default: 640,
+  })
+  .option('height', {
+    alias: 'y',
+    describe: 'Video height',
+    type: 'number',
+    default: 480,
+  })
+  .help('help')
+  .alias('help', 'h')
   .argv;
 
-const pipelineItems = [
-  'autovideosrc horizontal-speed=1 is-live=true',
-  'videoconvert',
-  'vp8enc cpu-used=5 deadline=1 keyframe-max-dist=10',
-  'queue leaky=1',
-  'm. webmmux name=m streamable=true',
-  'queue leaky=1',
-  `tcpserversink host=${argv.host} port=${argv.port} sync-method=2`,
-];
-
-const pipeline = new gstreamer.Pipeline(pipelineItems.join(' ! '));
-
-process.on('SIGINT', () => {
-  pipeline.stop();
-  process.exit();
+const stream = new VideoStream({
+  host: argv.host,
+  port: argv.port,
+  inputDevice: argv.device,
+  caps: {
+    width: argv.width,
+    height: argv.height,
+    framerate: argv.frames,
+  },
 });
 
-pipeline.play();
+stream.on('connect', () => console.log('Connected to server'));
+stream.on('play', () => console.log('Pulling video from source device'));
+stream.on('buffer', (buffer) => console.log(`Sending buffer to server (${buffer.length})`));
+stream.on('error', (err) => console.error('Stream error: ', err));
+
+process.on('SIGINT', () => {
+  stream.on('disconnect', () => {
+    console.log('Disconnected from server. Exiting...');
+    process.exit(0);
+  });
+  stream.disconnect();
+});
+
+stream.connect().then(() => {
+  stream.client.ref();
+  stream.transmit();
+});
